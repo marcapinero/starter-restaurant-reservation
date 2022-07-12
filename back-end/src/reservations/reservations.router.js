19ebfed4cca7613f5c09e250523e4a2dd130e82a
Marc Pinero <marcapinero@gmail.com>
2022-07-12 11:09:05 -0500

    first commit

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -4,9 +4,19 @@
  * @type {Router}
  */
 
-const router = require("express").Router();
-const controller = require("./reservations.controller");
-
-router.route("/").get(controller.list);
-
-module.exports = router;
+ const router = require("express").Router();
+ const controller = require("./reservations.controller");
+ 
+ router.route("/")
+   .get(controller.list)
+   .post(controller.create);
+ 
+ router.route("/:reservationId")
+   .get(controller.read)
+   .put(controller.update);
+ 
+ router.route("/:reservationId/status")
+   .put(controller.updateStatus);
+ 
+ 
+ module.exports = router;
--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -1,17 +1,18 @@
 import React from "react";
-
 import { Redirect, Route, Switch } from "react-router-dom";
 import Dashboard from "../dashboard/Dashboard";
-import NotFound from "./NotFound";
+import Reservations from "../reservations/Reservations";
+import Tables from "../tables/Tables";
+import Search from "../search/Search";
 import { today } from "../utils/date-time";
+import NotFound from "./NotFound";
 
 /**
  * Defines all the routes for the application.
  *
- * You will need to make changes to this file.
- *
  * @returns {JSX.Element}
  */
+
 function Routes() {
   return (
     <Switch>
@@ -20,6 +21,15 @@ function Routes() {
       </Route>
       <Route exact={true} path="/reservations">
         <Redirect to={"/dashboard"} />
+      </Route>
+      <Route path="/reservations">
+        <Reservations />
+      </Route>
+      <Route path="/tables">
+        <Tables />
+      </Route>
+      <Route path="/search">
+        <Search />
       </Route>
       <Route path="/dashboard">
         <Dashboard date={today()} />
